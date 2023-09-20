e509a7d766f20562d0073c78e265865b8d71c2f4
Thavorac <thavorac@sala.co>
2023-09-20 12:12:44 +0700

    Sample component

---
+++ e-commerce/src/components/basic/Button.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div v-if="color" :style="{ background: color }">
+      {{ icon }} | {{ text }}
+    </div>
+    <div v-else class="outline">{{ icon }} | {{ text }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["text", "icon", "color"],
+};
+</script>
+
+<style>
+.outline {
+  border: 1px solid grey;
+  border-radius: 5px;
+}
+</style>
