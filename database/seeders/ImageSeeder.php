b295a516de1469640ded8674d7e884de060f99ef
Thavorac <thavorac@sala.co>
2023-12-09 10:51:40 +0700

    Seeders for table categories, products, images

--- database/seeders/ImageSeeder.php
+++ database/seeders/ImageSeeder.php
@@ -4,18 +4,14 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Str;
 
-class CategorySeeder extends Seeder
+class ImageSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        DB::table('categories')->insert([
-            'name' => Str::random(10)
-        ]);
+        //
     }
 }
