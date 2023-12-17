d88f15e2f72ae47ed15498b21d5e1ae73077d1f3
Thachansopharong <sopharongtha13@gmail.com>
2023-12-17 10:00:26 +0700

    work with migration and seeder

--- ecommerce-backend/database/seeders/SupplySeeder.php
+++ ecommerce-backend/database/seeders/SupplySeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB; // Add this import statement
 
 class SupplySeeder extends Seeder
 {
@@ -12,6 +13,13 @@ class SupplySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('suppliers')->insert([
+            'name' => 'ABC',
+            'address' => 'Phnom penh',
+            'phone' => '093211233',
+            'email' => 'sopharongtha13@gmail.com',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
     }
 }
