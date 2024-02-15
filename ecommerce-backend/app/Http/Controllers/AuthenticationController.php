64437c4af5576f3ea9e7bc09e0bfa7c8a1fdf4e9
Thachansopharong <sopharongtha13@gmail.com>
2024-02-15 12:31:21 +0700

    Verify OTP

--- ecommerce-backend/app/Http/Controllers/AuthenticationController.php
+++ ecommerce-backend/app/Http/Controllers/AuthenticationController.php
@@ -7,36 +7,56 @@ use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\OtpMail; // Import the OtpMail class
+use Carbon\Carbon; // Import Carbon for date/time operations
+
 
 
 
 class AuthenticationController extends Controller
 {
-    public function register(Request $request){
-        $userFound = User::where("email",$request->get("email"))->first();
+   public function register(Request $request){
+    // 1. check if email if it is already exit or not 
+    $userFound = User::where("email",$request->get("email"))->first();
 
-        if($userFound){
-            return response(["Message"=>"This email already exist."],400);
+    if($userFound){
+        return response(["message"=>"The email is already exist!"],400);
+    }else{
+        if($request->get("password") == $request->get("comfirm_password")){
+
+            $user = new User();
+            $user->name = $request->get("username");
+            $user->email = $request->get("email");
+            $user->password = Hash::make($request->get("password"));
+            $user->otp = mt_rand(100000 , 999999);
+
+            $user->save();
+
+            //Send a 6 digits number to user's email, in format as this link:
+            //http://localhost:9000/verify_otp?code=xxxxxx
+            //http://localhost:80/verify_otp?user_id=xxxx&otp=xxxxxx
+            Mail::to($user->email)->send(new OtpMail("http://localhost:80/verify_otp?user_id=".$user->id."&otp=".$user->otp));
+            return response(["message"=>"good"]);
         }else{
-            if($request->get("password") == $request->get("comfirm_password")){
-
-                $user = new User();
-                $user->name = $request->get("username");
-                $user->email = $request->get("email");
-                $user->password = Hash::make($request->get("password"));
-                $user->otp = mt_rand(100000 , 999999);
-
-                // $user->save();
-
-                Mail::to($user->email)->send(new OtpMail("http://localhost:80/verify_otp?user_id=".$user->id."&opt=".$user->otp));
-                return response(["message"=>"good"]);
-
-   
-            }else{
-                return response(["Message"=>"Password and Confirm Password are not match ."],400);
-            }
-           
+            return response(["message"=>"Password and Comfirm_password are not match ."],400);
         }
         
     }
+   }
+
+   public function verifyOTP(Request $request){
+    $user = User::find($request->query('user_id'));
+
+    if($user){
+        if($user->otp == $request->query('otp')){
+            $user->email_verified_at = Carbon::now();
+            $user->save();
+
+            return response(["message"=>"Email is verified.You can start login."]);
+        }else{
+            return response(["message"=>"OTP is invalid"],400);
+        }
+    }else{
+        return response(["message"=>"User not found"],400);
+    }
+   }
 }
--- ecommerce-backend/routes/web.php
+++ ecommerce-backend/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthenticationController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// api url for Authentication 
+Route::get('/verify_otp',[AuthenticationController::class,'verifyOTP']);
